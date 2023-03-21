c4e7865ebe10132dd55cfdb40781ebd507a6895b
Bruno Ferreira <brunoferreira378@gmail.com>
2023-03-21 14:36:38 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Mail;
+using TesteFioTecBiblioteca.Services;
+
+Console.WriteLine("Hello, World!");
+
+// Define o caminho da pasta a ser varrida
+string pasta = @"C:\MinhaPasta";
+
+EmailService emailService = new EmailService();
+
+emailService.MensagemPodeInserir(pasta);
+
+// Fecha a conexão com o banco de dados
+emailService.FecharConexao();
+
+// Aguarda o usuário pressionar uma tecla para encerrar o programa
+Console.WriteLine("Processamento concluído. Pressione uma tecla para encerrar o programa.");
+Console.ReadKey();
